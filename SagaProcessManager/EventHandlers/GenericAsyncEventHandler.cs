3dff963cc9fde1afbe8caa5a037e43a186829475
makbari.it <makbari.it@outlook.com>
2019-06-16 16:58:13 +0430

    GenericAsyncEventHandler

---
+++ SagaProcessManager/EventHandlers/GenericAsyncEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Chronicle;
+using Common;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Core.DependencyInjection;
+
+namespace SagaProcessManager.EventHandlers
+{
+    public class GenericAsyncEventHandler<T> : IAsyncMessageHandler where T:class, IEvent
+    {
+        private readonly ISagaCoordinator _sagaCoordinator;
+        
+        public GenericAsyncEventHandler(ISagaCoordinator sagaCoordinator)
+        {
+            _sagaCoordinator = sagaCoordinator;
+        }
+
+        public Task Handle(string message, string routingKey)
+        {
+            return Handle(message);
+        }
+
+        private Task Handle(string message)
+        {
+            var context = SagaContext
+                .Create()
+                .WithCorrelationId(Guid.NewGuid())
+                .WithOriginator("Test")
+                .WithMetadata("key", "lulz")
+                .Build();
+
+            var note = JsonConvert.DeserializeObject<T>(message);
+
+            return !note.BelongsToSaga() ? Task.CompletedTask : _sagaCoordinator.ProcessAsync(note, context);
+        }
+    }
+}
---
+++ SagaProcessManager/EventHandlers/SagaEventHandlerDependencyInjectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Common;
+using Common.Events;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client.Core.DependencyInjection;
+
+namespace SagaProcessManager.EventHandlers
+{
+    public static class SagaEventHandlerDependencyInjectionExtensions
+    {
+        public static IServiceCollection AddEventHandlers(this IServiceCollection services)
+        {
+            var type = typeof(IEvent);
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract)
+                .ToList();
+
+            var genericAsyncMessageHandlerType = typeof(GenericAsyncEventHandler<>);
+
+            foreach (var item in types)
+            {
+                var attribute = item.GetCustomAttributes(typeof(MessageNamespaceAttribute), true).FirstOrDefault() as MessageNamespaceAttribute;
+
+                if (attribute is null)
+                {
+                    continue;
+                }
+                
+                var syncMessageHandlerType = genericAsyncMessageHandlerType.MakeGenericType(item);
+                var method = typeof(RabbitMqClientDependencyInjectionExtensions).GetMethod("AddAsyncMessageHandlerSingleton", new Type[] { typeof(IServiceCollection), typeof(string)});
+                var generic = method.MakeGenericMethod(syncMessageHandlerType);
+                generic.Invoke(services, new object[] {  services, attribute.Namespace });
+            }
+
+            return services;
+        }
+    }
+}
