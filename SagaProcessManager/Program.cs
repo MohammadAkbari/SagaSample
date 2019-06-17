5982f4a48f1fc75d0ab70b457734c5031e270ce1
makbari.it <makbari.it@outlook.com>
2019-06-17 14:51:36 +0430

    add log

--- SagaProcessManager/Program.cs
+++ SagaProcessManager/Program.cs
@@ -1,4 +1,5 @@
-﻿using Chronicle;
+﻿using System;
+using Chronicle;
 using Common;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,45 +13,54 @@ namespace SagaProcessManager
 {
     class Program
     {
+
+        
         public static void Main(string[] args)
         {
+
             var builder = new HostBuilder()
-                  .ConfigureAppConfiguration((hostingContext, config) =>
-                  {
-                      config.AddJsonFile("appsettings.json", optional: true);
-                  })
-                  .ConfigureServices((hostContext, services) =>
-                  {
-                      var configuration = hostContext.Configuration;
-
-                      services.AddChronicle();
-
-                      var rabbitMqSection = configuration.GetSection("RabbitMq");
-                      var exchangeSection = configuration.GetSection("RabbitMqExchange");
-
-                      services.AddRabbitMqClient(rabbitMqSection)
-                          .AddExchange("exchange.name", exchangeSection)
-                          .AddEventHandlers();
-
-                      services.AddHostedService<QueueHostedService>();
-                  })
-                  .ConfigureLogging((hostingContext, logging) =>
-                  {
-                      var configuration = hostingContext.Configuration;
-
-                      logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                      
-                      logging.AddSerilog();
-                      
-                      Log.Logger = new LoggerConfiguration()
-                          .Enrich.FromLogContext()
-                          .Enrich.WithMachineName()
-                          .Enrich.WithProperty("Assembly", System.AppDomain.CurrentDomain.FriendlyName)
-                          .WriteTo.Seq(configuration["Seq:ServerUrl"])
-                          .CreateLogger();
-                  });
+                  .ConfigureAppConfiguration(ConfigureAppConfiguration)
+                  .ConfigureServices(ConfigureServices)
+                  .ConfigureLogging(ConfigureLogging);
             
             builder.RunConsoleAsync().GetAwaiter().GetResult();
         }
+        
+        private static readonly Action<HostBuilderContext, IServiceCollection> ConfigureServices = (hostContext, services) =>
+        {
+            var configuration = hostContext.Configuration;
+
+            services.AddChronicle();
+
+            var rabbitMqSection = configuration.GetSection("RabbitMq");
+            var exchangeSection = configuration.GetSection("RabbitMqExchange");
+
+            services.AddRabbitMqClient(rabbitMqSection)
+                .AddExchange("exchange.name", exchangeSection)
+                .AddEventHandlers();
+
+            services.AddHostedService<QueueHostedService>();
+        };
+        
+        private static readonly Action<HostBuilderContext, ILoggingBuilder> ConfigureLogging = (hostingContext, logging) =>
+        {
+            var configuration = hostingContext.Configuration;
+
+            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                      
+            logging.AddSerilog();
+                      
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Assembly", System.AppDomain.CurrentDomain.FriendlyName)
+                .WriteTo.Seq(configuration["Seq:ServerUrl"])
+                .CreateLogger();
+        };
+        
+        private static readonly Action<HostBuilderContext, IConfigurationBuilder> ConfigureAppConfiguration = (hostingContext, config) =>
+        {
+            config.AddJsonFile("appsettings.json", optional: true);
+        };
     }
 }
